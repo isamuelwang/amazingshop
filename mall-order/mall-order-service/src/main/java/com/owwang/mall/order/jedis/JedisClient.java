78d9577ad352bd30de2b0eed78096e058bc3901d
isamuelwang <isamuelwang@gmail.com>
2020-01-03 18:39:28 +0800

    订单系统环境搭建

---
+++ mall-order/mall-order-service/src/main/java/com/owwang/mall/order/jedis/JedisClient.java
@@ -0,0 +1,20 @@
+package com.owwang.mall.order.jedis;
+
+
+import java.util.Map;
+
+public interface JedisClient {
+
+	String set(String key, String value);
+	String get(String key);
+	Boolean exists(String key);
+	Long expire(String key, int seconds);
+	Long ttl(String key);
+	Long incr(String key);
+	Long hset(String key, String field, String value);
+	String hget(String key, String field);
+	Map<String, String> hgetAll(String key);
+	Long hdel(String key, String... field);//删除hkey
+
+	
+}
